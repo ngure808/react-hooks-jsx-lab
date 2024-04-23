6360443a596ef52ce185d3781dd24a7e92744106
ngure808 <mwangingure808@gmail.com>
2024-04-23 19:32:36 +0300

    added a h1 tag with some styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,15 @@
 import React from "react";
 import { name, city } from "../data/data.js";
 
+const textColor = {
+  color : "firebrick"
+}
+
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return <div id="home">Home
+  <h1 style={textColor}>{name} is a Web Developer from {city}</h1>
+  </div>;
 }
 
 export default Home;
